528d6c6b781dfaec6dd837db3c1c935794a469ac
Alain Stulz <alain@appswithlove.com>
2017-03-08 15:21:47 +0100

    Improve Tests in RollAgainSquareTest by adding message instead of comment

--- exercise_02/test/snakes/RollAgainSquareTest.java
+++ exercise_02/test/snakes/RollAgainSquareTest.java
@@ -22,8 +22,8 @@ public class RollAgainSquareTest extends SquareTest {
 		assertEquals(1, jack.position());
 		assertEquals(1, jill.position());
 		game.movePlayer(6); // moves Jack
-		assertTrue(jack.position() > TESTSQUAREPOSITION); // should move at least one over RollAgainSquare
-		assertTrue(jack.position() <= TESTSQUAREPOSITION + Die.FACES); // should move no more than die allows
+		assertTrue("Jack moves at least one over RollAgainSquare", jack.position() > TESTSQUAREPOSITION);
+		assertTrue("Jack moves no more than die allows", jack.position() <= TESTSQUAREPOSITION + Die.FACES);
 		assertEquals(1, jill.position());
 		int jackPosition = jack.position();
 		game.movePlayer(5); // moves Jill
