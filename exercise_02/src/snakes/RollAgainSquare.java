7c1360841218b5a82a11b990d02662a806ac069b
Samuel Schwegler <samuel.schwegler@gmx.ch>
2017-03-09 22:15:30 +0100

    added the RollAgain string description

--- exercise_02/src/snakes/RollAgainSquare.java
+++ exercise_02/src/snakes/RollAgainSquare.java
@@ -1,11 +1,29 @@
 package snakes;
 
-/**
- * Created by ast on 08.03.17.
+/*
+ * This is the rollAgain class
+ * The methods are inherited from Square and overridden to specify this sub-class
+ * the player get a second die roll
+ *
+ * @author Samuel Schwegler 16-119-695
+ * @author Alain Stulz
+ *
+ * @param game							Game instance of the Game class
+ * @param position						int value of actual position
+ *
+ * @see Square							The mother-class
+ * @see Game							Game class
+ * @see landHereOrGoHome()				checks if the second roll is ok
  */
+
 public class RollAgainSquare extends Square {
     public RollAgainSquare(Game game, int position) {
         super(game, position);
+    }
+
+    @Override
+    protected String squareLabel() {
+        return position + " (RollAgain)";
     }
 
     @Override
