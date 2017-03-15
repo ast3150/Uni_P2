48cf220c8488fe01b04c657cfb36e108a1730950
Alain Stulz <alain@appswithlove.com>
2017-03-15 23:48:12 +0100

    Add SouthEastCommand: Bugfixes in CommandParser, ICommand

---
+++ exercise_03/src/turtle/SouthEastCommand.java
@@ -0,0 +1,42 @@
+package turtle;
+
+/**
+ * Created by ast on 14.03.17.
+ */
+public class SouthEastCommand extends Command implements ICommand {
+
+    public static boolean canHandle(String instruction) {
+        System.out.println(instruction.matches("south east ([0-9])*"));
+        return (instruction.matches("south east ([0-9])*"));
+    }
+
+    public void parseFromString(String instruction) {
+        String distance_str = instruction.replaceAll("[^0-9]", "");
+        distance = Integer.parseInt(distance_str);
+    }
+
+    public boolean[][] executeOn(boolean[][] board, int lastColumn, int lastRow) throws ArrayIndexOutOfBoundsException, IndexOutOfBoundsException {
+        this.lastColumn = lastColumn;
+        this.lastRow = lastRow;
+
+        if (lastColumn >= board[lastRow].length) throw new ArrayIndexOutOfBoundsException();
+        if (lastRow >= board.length) throw new ArrayIndexOutOfBoundsException();
+
+        int i = lastColumn;
+        for (int j = lastRow; j < lastRow + distance; j++) {
+            if (i < 0) throw new IndexOutOfBoundsException();
+            if (i >= board[j].length) throw new IndexOutOfBoundsException();
+
+            if (j < 0) throw new IndexOutOfBoundsException();
+            if (j >= board.length) throw new IndexOutOfBoundsException();
+
+            board[i][j] = true;
+            i++;
+        }
+
+        this.lastRow = lastRow + distance;
+        this.lastColumn = lastColumn + distance;
+
+        return board;
+    }
+}
