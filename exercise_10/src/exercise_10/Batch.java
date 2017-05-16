cd92237ec05249dec328ce402fe0d02ee0d263d5
Claudio Corrodi <corrodi.claudio@gmail.com>
2017-05-16 21:24:59 +0200

    Add exercise 10

---
+++ exercise_10/src/exercise_10/Batch.java
@@ -0,0 +1,12 @@
+package exercise_10;
+
+public class Batch {
+	public void addBook(Book book, int quantity) {
+		throw new NotImplementedException();
+	}
+
+	/* Nested batches are also possible! */
+	public void addBatch(Batch batch, int quantity) {
+		throw new NotImplementedException();
+	}
+}
