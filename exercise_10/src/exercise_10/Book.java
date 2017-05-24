87e09767c040cefe9712c99dd667f885eac611a9
Samuel Schwegler <samuel.schwegler@gmx.ch>
2017-05-24 18:01:29 +0200

    imported the libraries

--- exercise_10/src/exercise_10/Book.java
+++ exercise_10/src/exercise_10/Book.java
@@ -1,7 +1,16 @@
 package exercise_10;
 
+/*
+* yeah a Book
+*
+ */
 public class Book {
+	String title;
+	String authors;
+	double cost;
 	public Book(String title, String authors, double cost) {
-		throw new NotImplementedException();
+		this.title = title;
+		this.authors = authors;
+		this.cost = cost;
 	}
 }
